4a9ef67cb41ec72bf568c23986f071032beb9c43
AaronChoi <aaronschoilcw@gmail.com>
2021-04-06 11:04:53 -0700

    Completed POST/create for /order ::: resolve #7

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -8,9 +8,77 @@ const nextId = require("../utils/nextId");
 
 // TODO: Implement the /orders handlers needed to make the tests pass
 
+//POST:create on /orders
+const postHasDeliverTo = (req, res, next) => {
+    const { data : { deliverTo } = {} } = req.body;
+    if(deliverTo){
+        next();
+    }
+    else{
+        next({
+            status: 400,
+            message: "Order must include a deliverTo"
+        });
+    }
+};
+
+const postHasMobileNumber = (req, res, next) => {
+    const { data : { mobileNumber } = {} } = req.body;
+    if(mobileNumber){
+        next();
+    }
+    else{
+        next({
+            status: 400,
+            message: "Order must include a mobileNumber"
+        });
+    }
+};
+
+const postHasDishes = (req, res, next) => {
+    const { data : { dishes } = {} } = req.body;
+    if(dishes && dishes.length > 0 && Array.isArray(dishes)){
+        next();
+    }
+    else{
+        next({
+            status: 400,
+            message: "Order must include at least one dish"
+        });
+    }
+};
+
+const postHasDishesWithQuantities = (req, res, next) => {
+    const { data : { dishes } = {} } = req.body;
+    for(const [index, dish] of dishes.entries()){
+        if(dish.quantity && dish.quantity > 0 && Number.isInteger(dish.quantity)){
+            console.log(`dish at index ${index} passes the quantity test`)
+        }
+        else{
+            return next({
+                status: 400,
+                message: `Dish ${index} must have a quantity that is an integer greater than 0`
+            })
+        }
+    }
+    console.log("all dishes passed this test")
+    return next();
+};
+//------------------
+
 //CRUDL Functions:
 const create = (req, res, next) => {
-
+    const { data : { deliverTo, mobileNumber, status, dishes } = {} } = req.body;
+    const newId = nextId();
+    const newOrder = {
+        id: newId,
+        deliverTo,
+        mobileNumber,
+        status,
+        dishes
+    };
+    orders.push(newOrder);
+    res.status(201).json({ data : newOrder })
 };
 
 const read = (req, res, next) => {
@@ -30,7 +98,7 @@ const list = (req, res, next) => {
 };
 
 module.exports = {
-    create: [],
+    create: [postHasDeliverTo, postHasMobileNumber, postHasDishes, postHasDishesWithQuantities, create],
     read: [],
     update: [],
     destory: [],
